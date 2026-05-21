a9ee9fe0b800219f60a01b467c0e86832250c87e
SamanuelAdmin <samoolenkosvatik93@gmail.com>
2026-05-21 16:05:47 +0000

    pk triggers

--- views.sql
+++ views.sql
@@ -1,4 +1,4 @@
-CREATE VIEW entrant_view AS
+CREATE OR REPLACE VIEW entrant_view AS
 SELECT 
   e.FullName,
   e.CEI,
@@ -8,34 +8,34 @@ SELECT
   e.BDate
 FROM Entrant e; 
 
-CREATE VIEW spec_test_view AS
+CREATE OR REPLACE VIEW spec_test_view AS
 SELECT
   st.HeiSpecId,
   st.TestId
 FROM SpecTest st; 
 
-CREATE VIEW hei_spec_view AS
+CREATE OR REPLACE VIEW hei_spec_view AS
 SELECT
   hs.HeiId,
   hs.SpecId
 FROM HeiSpec hs;
 
-CREATE VIEW ent_test_view AS
+CREATE OR REPLACE VIEW ent_test_view AS
 SELECT 
   et.Name
 FROM EntTest et;
 
-CREATE VIEW spec_view AS
+CREATE OR REPLACE VIEW spec_view AS
 SELECT 
   s.Name
 FROM Spec s;
 
-CREATE VIEW hei_view AS
+CREATE OR REPLACE VIEW hei_view AS
 SELECT 
   h.Name
 FROM Hei h;
 
-CREATE VIEW comp_educ_inst_view AS
+CREATE OR REPLACE VIEW comp_educ_inst_view AS
 SELECT
   cei.Name
 FROM CompEducInst cei;
