7c96052a603aa3b059e5e8a427d1e2edd6099445
SamanuelAdmin <samolenkosvatik93@gmail.com>
2026-05-25 21:41:12 +0000

    Login page final

--- EntrantCRM/Pages/Login.cshtml.cs
+++ EntrantCRM/Pages/Login.cshtml.cs
@@ -24,32 +24,29 @@ public class LoginModel : PageModel
         HttpContext.Session.SetString("password", password);
         HttpContext.Session.SetString("link", link);
 
-        // TODO: Check the creds before adding
         Oracle.ManagedDataAccess.Client.OracleConnectionStringBuilder ria = new Oracle.ManagedDataAccess.Client.OracleConnectionStringBuilder(); // Створюємо підключення
 
         string serviceName = link.Replace("/", "").Replace("\\", "").Trim();
-        string tnsConnectionString = $"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORA)))";
-        string finalConnectionString = $"User Id=Scott;Password=tiger1;Data Source={tnsConnectionString};";
+        string tnsConnectionString = $"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=serveroracle)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME={link})))";
+        // string finalConnectionString = $"User Id=Scott;Password=tiger1;Data Source={tnsConnectionString};";
 
 
-        ria.UserID = login;  // Присвоюємо введенний ID користувача з textBox1
-        if (login.ToLower() == "sys")
+        ria.UserID = login; 
+        ria.Password = password;  
+        ria.DataSource = tnsConnectionString;
+        ria.PersistSecurityInfo = true;
+
+        Oracle.ManagedDataAccess.Client.OracleConnection con = new Oracle.ManagedDataAccess.Client.OracleConnection();
+        con.ConnectionString = ria.ConnectionString;
+
+
+        try 
         {
-            finalConnectionString += "DBA Privilege=SYSDBA;";
-        }
-
-        ria.Password = password;   // Присвоюємо введенний пароль з textBox2
-        ria.PersistSecurityInfo = true;  // Дозволяємо зберігати інформацію про безпеку
-
-        Oracle.ManagedDataAccess.Client.OracleConnection con = new Oracle.ManagedDataAccess.Client.OracleConnection(finalConnectionString);
-        con.ConnectionString = ria.ConnectionString; // Встановлюємо рядок підключення
-        //try  // Спроба під'єднання
-        //{
-            con.Open();  // Відкриваємо з'єднання з базою даних
+            con.Open(); 
             return RedirectToPage("/Index");
-        //} catch {
-        //  _logger.LogError("Cannot connect to database!");
-        //  return new BadRequestResult();
-        //}
+        } catch {
+          _logger.LogError("Cannot connect to database!");
+          return new BadRequestResult();
+        }
     }
 }
