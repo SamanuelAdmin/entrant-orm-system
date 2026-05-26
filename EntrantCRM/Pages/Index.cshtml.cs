e79f5a6088b8c8506286e27c3e2a1d22431cb790
SamanuelAdmin <samoolenkosvatik93@gmail.com>
2026-05-26 15:16:50 +0000

    [UNTESTED] Main page showing and adding table rows

--- EntrantCRM/Pages/Index.cshtml.cs
+++ EntrantCRM/Pages/Index.cshtml.cs
@@ -14,6 +14,7 @@ public class IndexModel : PageModel
 
     public DataTable table;
     public string title;
+    public string viewname;
 
 
     public string GetConnectionString(string login, string password, string link) {
@@ -68,12 +69,15 @@ public class IndexModel : PageModel
         if (viewname== null) {
             viewname = "entrant_view" // default view
         }
+        this.viewname = viewname; // info for actions
+
         if (title == null) {
             this.title = viewname.Replace("_view", "");
         } else {
             this.title = title;
         }
         
+
         try 
         {
             this.table = GetViewData(connectionString, viewname);
@@ -83,6 +87,65 @@ public class IndexModel : PageModel
         }
 
         return Page();
+    }
+
+
+    public IActionResult OnPost(string tableName) {
+        var login = HttpContext.Session.GetString("login");
+        if (login == null) { 
+            return RedirectToPage("/Login");
+        }
+
+        var password = HttpContext.Session.GetString("password");
+        var link = HttpContext.Session.GetString("link");
         
+        string connectionString = GetConnectionString(login, password, link)
+
+        // adding new string
+        var columnsToInsert = new List<string>();
+        var parameterNames = new List<string>();
+        var valuesDictionary = new Dictionary<string, string>();
+
+        foreach (var key in Request.Form.Keys)
+        {
+            if (key == "__RequestVerificationToken" || key == "tableName") continue;
+
+            columnsToInsert.Add(key);
+            parameterNames.Add($":{key}");
+            valuesDictionary.Add(key, Request.Form[key]);
+        }
+
+        if (columnsToInsert.Count == 0)
+        {
+            _logger.LogInformation("There is nothing to add!");
+            return Page();
+        }
+
+        string sql = $"INSERT INTO {tableName} ({string.Join(", ", columnsToInsert)}) VALUES ({string.Join(", ", parameterNames)})";
+
+        try
+        {
+            using (OracleConnection conn = new OracleConnection(_connectionString))
+            {
+                using (OracleCommand cmd = new OracleCommand(sql, conn))
+                {
+                    // defend from SQL injections
+                    foreach (var kvp in valuesDictionary)
+                    {
+                        cmd.Parameters.Add(new OracleParameter(kvp.Key, kvp.Value));
+                    }
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation($"Error when adding new line to {tableName}: " + ex.Message);
+            return new BadRequestResult();
+        }
+
+        return RedirectToPage();
     }
 }
